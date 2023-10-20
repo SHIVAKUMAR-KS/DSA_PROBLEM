6106a59fc78f50913e9fc6ea19d75bcdbe0da263
shiva kumar <115865333+SHIVAKUMAR-KS@users.noreply.github.com>
2023-10-20 12:26:23 +0530

    Add files via upload

---
+++ Queue/queueUsingStack.java
@@ -0,0 +1,53 @@
+package Queue;
+//implimentation of Queue data structure using stack
+//time complexity of enQueue : O(1)
+//time complexity of deQueue :O(n)
+//space complexity: O(n)
+import java.util.Stack;
+
+public class queueUsingStack {
+    static Stack<Integer> stack1 =new Stack<>();
+    static Stack<Integer> stack2=new Stack<>();
+
+    //insertion of the data in the queue
+    //time complexity : O(1)
+    static void enQueue(int data){
+        stack1.push(data);
+    }
+
+    //deletion of the data from the queue -FIFO
+    static int deQueue(){
+        int ele;
+        if(stack1.empty() && stack2.empty()){
+            System.out.println("Queue is empty");
+            System.exit(0);
+        }
+
+        //push all the element from stack1 to stack2
+        if(stack2.empty()){
+            while (!stack1.empty()){
+                ele=stack1.pop();
+                stack2.push(ele);
+            }
+        }
+        //pop the top element from stack2
+        //FIFO -first in first out
+        ele =stack2.pop();
+        return ele;
+
+    }
+    public static void main(String[] args) {
+        queueUsingStack q=new queueUsingStack();
+
+        //function calling for insertion in the queue
+        q.enQueue(1);
+        q.enQueue(2);
+        q.enQueue(3);
+        q.enQueue(4);
+
+        //function calling for deletion of an element from the queue
+        System.out.println("Deleted element is :"+q.deQueue());
+        System.out.println("Deleted element is :"+q.deQueue());
+        //q.deQueue();
+    }
+}
